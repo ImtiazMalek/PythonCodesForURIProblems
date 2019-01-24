dd8cfce6311f95cb19635bfc717f3bf55e1ea477
ImtiazMalek <46179788+ImtiazMalek@users.noreply.github.com>
2019-01-25 05:38:36 +0600

    finding average or some numbers

---
+++ Task1005.py
@@ -0,0 +1,9 @@
+A=float(input('enter a number: '))
+B=float(input('enter a number: '))
+C=float(input('enter a number: '))
+num1="%.1f"%A
+num2="%.1f"%B
+num3="%.1f"%C
+sum=float(num1)+float(num2)+float(num3)
+ans=(sum/3)
+print('MEDIA = %s'%"%.1f"%ans)
